c278972a4530f3ea7ea2d4a30fe20cee7e67892c
Pawel Wieladek <pawelwieladek@gmail.com>
2015-05-16 15:57:20 +0200

    Stream write

--- generate/src/app.js
+++ generate/src/app.js
@@ -4,15 +4,22 @@ var moment = require("moment");
 var Payload = require("./payload");
 
 var payload = new Payload({
-  amplitude: 4000,
-  minimum: 500
+  amplitude: 10000,
+  minimum: 20000
 });
 
 var now = moment();
 var logs = payload.logs(now.year(), now.month(), now.date(), now.hour(), now.minute());
 var filename = path.join(__dirname, "../target/generated-" + now.format("YYYY-MM-DD-HH-mm") + ".txt");
-var content = logs.join("\n");
+console.log(filename);
 console.log({ hour: now.hour(), minute: now.minute(), size: logs.size });
-fs.writeFile(filename, content, function(err) {
-  if (err) throw err;
+
+var stream = fs.createWriteStream(filename);
+stream.once("open", function(fd) {
+  logs.forEach((log, index) => {
+    stream.write(log.toString());
+    if (index % 1000 === 0) process.stdout.write(".");
+  });
+  process.stdout.write("\n");
+  stream.end();
 });
