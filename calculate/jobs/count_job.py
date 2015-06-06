f856c3eef53c00a109c5dbdcea7208deafaedf62
Disco <disco@node01.(none)>
2015-06-06 16:27:32 +0200

    Job working

--- calculate/jobs/count_job.py
+++ calculate/jobs/count_job.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 from disco.core import Job, result_iterator
 
 def map(line, params):
@@ -13,9 +14,11 @@ def reduce(iter, params):
         yield word, sum(counts)
 
 if __name__ == "__main__":
+    pattern = re.compile("^logs-")
     current_directory = os.path.realpath(os.path.dirname(os.path.realpath(__file__))) + "/"
     input_directory = os.path.normpath(current_directory + "../../generate/target/")
-    input_files = [file for file in os.listdir(input_directory) if os.path.isfile(os.path.join(input_directory, file))]
+    input_files = [input_directory + "/" + file for file in os.listdir(input_directory) 
+                   if pattern.match(file) and os.path.isfile(os.path.join(input_directory, file))]
     job = Job().run(required_files=[os.path.normpath(current_directory + "../model/log.py")],
                     input=input_files,
                     map=map,
