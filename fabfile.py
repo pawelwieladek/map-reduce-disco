6b6f97790f1635bc7870986145cc7463fb1bcacc
Pawel Wieladek <pawelwieladek@gmail.com>
2015-06-06 16:44:21 +0200

    Fabric run

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,24 @@
 import os
 from fabric.api import local
+from fabric.context_managers import cd
 from datetime import datetime
 
-def run():
-    current_directory = os.path.realpath(os.path.dirname(os.path.realpath(__file__)))
-    local("cd generate")
-    local("npm install")
-    local("gulp build")
-    local("cd ..")
+current_directory = os.path.realpath(os.path.dirname(os.path.realpath(__file__)))
 
+def clean():
+    with cd("generate"):
+        local("npm install")
+        local("gulp build")
+
+def generate():
     generate_task = current_directory + "/generate/dist/app.js"
     local("node " + generate_task + " --verbose")
 
+def calculate():
     calculate_task = current_directory + "/calculate/jobs/count_job.py"
     local("python " + calculate_task)
+
+def run():
+    clean()
+    generate()
+    calculate()
